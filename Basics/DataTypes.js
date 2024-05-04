1302d54ec6de20dd2ebab0fa3fd3d92320fb687c
Karim_Hadia <138756125+HadiaKarim16@users.noreply.github.com>
2024-05-04 09:42:54 +0000

    Datatypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,31 @@
+// Treat All JS code as newer version
+"use strict"; 
+
+//Datatypes
+let name = "Ali Khan"; // String
+let Marks = 50; // number
+let isPass = true; //boolean
+let Grade; // Undefined
+let temperature = null;
+
+
+// Typeof => use to know the datatype of any variable 
+console.log(typeof(name));
+console.log(typeof(Marks));
+console.log(typeof(isPass));
+console.log(typeof(Grade));
+console.log(typeof(temperature)); // Null is an object
+
+/* --------- Primitive Datatypes -------------*/
+
+// String => use to store text in single quote/ double quote
+// Big int => use to store large integer values
+// Number => stor values 2 power 53
+// Null => standalone value
+// undefined => uninitialized
+// Null => assigned to any variable null/empty value
+// Symbol =>  used for uniqueness in react 
+
+/* ---------- Non Primitive Datatypes ------------*/
+
+// Object
