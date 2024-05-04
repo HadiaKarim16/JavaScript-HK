ed7609135f5b9f4751f7e919af4377493bbaada5
Karim_Hadia <138756125+HadiaKarim16@users.noreply.github.com>
2024-05-04 09:18:05 +0000

    Varaibles in JS

---
+++ Basics/Variables.js
@@ -0,0 +1,7 @@
+const account_ID = 14445;
+let account_Email ="Hadiakarim@gmail.com";
+var account_Password = "12345";
+let account_State;
+console.log(account_ID);
+console.log(account_Email);
+console.table([account_Email,account_ID,account_Password,account_State]);
